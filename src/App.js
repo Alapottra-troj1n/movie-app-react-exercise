80555abbc0ed3593b87b30d85f9d00768e286b9a
Alapottra <alapottratrojon@gmail.com>
2022-03-05 18:37:19 +0600

    added api-key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// f4a738de
+
+const API_URL = 'http://www.omdbapi.com/?apikey=f4a738de'
 
 const App = () => {
+
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        console.log(data.movies);
+    }
+
+
+    useEffect(() =>{
+        searchMovies('superman');
+    },[])
+
+
+
     return(
         <h1>App</h1>
     );
